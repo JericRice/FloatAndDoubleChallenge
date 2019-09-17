73160908dd014f4f7888690cde4242dcc2d2b0ee
JericRice <rice.joseph@titans.easternflorida.edu>
2019-09-17 00:38:56 -0400

    Challenge to demonstrate compentency on Float and Double Precision

---
+++ src/com/josephrice/Main.java
@@ -0,0 +1,23 @@
+
+package com.josephrice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f; // f not required to indicate floating point but is best practice
+        double myDoubleValue = 5.25d; // d not required to indicate a double but is best practice
+    }
+}
